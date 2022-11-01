9c0fd43e7a51ebb2f505983d0c2f975e1c315315
MKHLink <[likhonhasankomol@gmail.com]>
2022-11-01 01:29:03 -0400

    added login for users

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -45,6 +45,31 @@ router.post('/',(req,res)=>{
         });
 });
 
+router.post('/login',(req,res)=>{
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData=>{
+        if(!dbUserData)
+        {
+            res.status(400).json({message:"Invalid email"});
+            return;
+        }
+
+        //res.json({user: dbUserData});
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if(!validPassword)
+        {
+            res.status(400).json({message: "Incorrect password"});
+        }
+
+        res.json({user: dbUserData, message:"Logged in!"})
+    });
+});
+
+
 router.put('/:id',(req,res)=>{
     User.update(req.body, {
         individualHooks: true,
